aaee8618f4bc012e9e3686cdd0b14ad2cfad7a62
rujaraju <jan.rundt@gmail.com>
2017-05-12 11:12:43 +0000

    finished

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ var app = express();
 var path = require('path');
 var mongo = require('mongodb').MongoClient;
 
-app.get('/urlshortener1', function (req, res) {
-  res.send('/home/ubuntu/workspace/urlshortener/hello.html');
-});
-
  app.get('/urlshortener', function(req, res) {
      res.sendFile('/home/ubuntu/workspace/urlshortener/hello.html');
  });
@@ -14,26 +10,35 @@ app.get('/urlshortener1', function (req, res) {
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'templates'));
 
-app.get('/urlshortener/:data', function (req, res) {
-   var url = req.params.data;
-
+app.get('/urlshortener/:url(*)', function (req, res) {
+   //var url = req.params.data;
+   var url = req.params.url;
   
   mongo.connect('mongodb://localhost:27017/URLs', function(err, db) {
       if (err) throw err;
       if (db) {
-         res.send(url + "funkar"); 
-        /*var collection = db.collection("prices");
-      collection.aggregate([
-       {$match: {size: size}},
-       {$group: {_id: "prices", average: {$avg: '$price'}}}
-       ]).toArray(function(err, results){
-        if (err) throw err;
-        console.log(results[0].average.toFixed(2));
-        db.close();
-       });
-      
-      */}  
-    }) 
+          if (url.substring(0,8) == "https://" || url.substring(0,7) == "http://")
+          {db.collection("url").count({}, 
+          function(err, count){if (err) throw err;
+          var shortUrl = "https://api-developments-ruja.c9users.io/urlshortener/" + (count + 1);
+          var toAdd = {"original_url": url, 
+          "short_url": shortUrl};
+          res.send(JSON.stringify(toAdd));
+          db.collection("url").insert(toAdd);
+              
+          });
+         
+      }
+      else {db.collection("url").count({"short_url" : "https://api-developments-ruja.c9users.io/urlshortener/" + url}, function(err, count) {
+      if (err) throw err;
+      if (count == 0) {res.send("There is no such url, I'm afraid...")}
+      else {db.collection("url").find(
+          {"short_url" : "https://api-developments-ruja.c9users.io/urlshortener/" + url}).forEach(
+              function(data){res.redirect(data.original_url)})}
+      });
+          }
+      }  
+    }); 
 });
 
 app.listen(8080, function () {
